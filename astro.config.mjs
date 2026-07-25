f1a710a882531cccc142c76751693f053e71c3f5
quick <quick@quickdeMacBook-Pro.local>
2026-07-25 17:57:56 +0800

    Fix GitHub Pages deployment

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,7 +1,6 @@
-// @ts-check
-import { defineConfig } from 'astro/config';
+import { defineConfig } from "astro/config";
 
 export default defineConfig({
-  site: 'https://quikchanchan.github.io',
-  base: '/quickchan-website',
+  site: "https://quikchanchan.github.io",
+  base: "/quickchan-website",
 });
