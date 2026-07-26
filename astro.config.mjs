c19d27a5a0e194d8009c84e61ce8a07fd9738472
Quick Chan <quickchan@hotmail.com>
2026-07-27 01:14:35 +0800

    Fix Astro base path for custom domain

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,6 +1,5 @@
 import { defineConfig } from "astro/config";
 
 export default defineConfig({
-  site: "https://quikchanchan.github.io",
-  base: "/quickchan-website",
+  site: "https://www.hywsource.com",
 });
