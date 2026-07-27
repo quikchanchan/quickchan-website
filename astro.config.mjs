ebae21e6dcd7dd86f648bc0da021c2fd18aaf13c
Quick Chan <quickchan@hotmail.com>
2026-07-27 14:17:53 +0800

    Add sitemap generation for SEO

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,5 +1,9 @@
 import { defineConfig } from "astro/config";
+import sitemap from "@astrojs/sitemap";
 
 export default defineConfig({
   site: "https://www.hywsource.com",
+  integrations: [
+    sitemap()
+  ],
 });
